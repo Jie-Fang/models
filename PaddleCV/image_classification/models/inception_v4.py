43604f672c11796dfbffeb830898e1c7fd59cec0
Jie Fang <jeffreyfang1992@163.com>
2019-11-11 15:50:57 +0800

    add NHWC for InceptionV4

--- PaddleCV/image_classification/models/inception_v4.py
+++ PaddleCV/image_classification/models/inception_v4.py
@@ -30,22 +30,22 @@ class InceptionV4():
 
         pass
 
-    def net(self, input, class_dim=1000):
-        x = self.inception_stem(input)
+    def net(self, input, args, class_dim=1000):
+        x = self.inception_stem(input, data_format=args.data_format)
 
         for i in range(4):
-            x = self.inceptionA(x, name=str(i + 1))
-        x = self.reductionA(x)
+            x = self.inceptionA(x, name=str(i + 1), data_format=args.data_format)
+        x = self.reductionA(x, data_format=args.data_format)
 
         for i in range(7):
-            x = self.inceptionB(x, name=str(i + 1))
-        x = self.reductionB(x)
+            x = self.inceptionB(x, name=str(i + 1), data_format=args.data_format)
+        x = self.reductionB(x, data_format=args.data_format)
 
         for i in range(3):
-            x = self.inceptionC(x, name=str(i + 1))
+            x = self.inceptionC(x, name=str(i + 1), data_format=args.data_format)
 
         pool = fluid.layers.pool2d(
-            input=x, pool_type='avg', global_pooling=True)
+            input=x, pool_type='avg', global_pooling=True, data_format=args.data_format)
 
         drop = fluid.layers.dropout(x=pool, dropout_prob=0.2)
 
@@ -69,7 +69,8 @@ class InceptionV4():
                       padding=0,
                       groups=1,
                       act='relu',
-                      name=None):
+                      name=None,
+                      data_format='NCHW'):
         conv = fluid.layers.conv2d(
             input=data,
             num_filters=num_filters,
@@ -80,7 +81,8 @@ class InceptionV4():
             act=None,
             param_attr=ParamAttr(name=name + "_weights"),
             bias_attr=False,
-            name=name)
+            name=name,
+            data_format=data_format)
         bn_name = name + "_bn"
         return fluid.layers.batch_norm(
             input=conv,
@@ -89,257 +91,305 @@ class InceptionV4():
             param_attr=ParamAttr(name=bn_name + "_scale"),
             bias_attr=ParamAttr(name=bn_name + "_offset"),
             moving_mean_name=bn_name + '_mean',
-            moving_variance_name=bn_name + '_variance')
+            moving_variance_name=bn_name + '_variance',
+            data_format=data_format)
 
-    def inception_stem(self, data, name=None):
+    def inception_stem(self, data, name=None, data_format='NCHW'):
         conv = self.conv_bn_layer(
-            data, 32, 3, stride=2, act='relu', name="conv1_3x3_s2")
-        conv = self.conv_bn_layer(conv, 32, 3, act='relu', name="conv2_3x3_s1")
+            data, 32, 3, stride=2, act='relu', name="conv1_3x3_s2", data_format=data_format)
+        conv = self.conv_bn_layer(conv, 32, 3, act='relu', name="conv2_3x3_s1", data_format=data_format)
         conv = self.conv_bn_layer(
-            conv, 64, 3, padding=1, act='relu', name="conv3_3x3_s1")
+            conv, 64, 3, padding=1, act='relu', name="conv3_3x3_s1", data_format=data_format)
 
         pool1 = fluid.layers.pool2d(
-            input=conv, pool_size=3, pool_stride=2, pool_type='max')
+            input=conv, pool_size=3, pool_stride=2, pool_type='max', data_format=data_format)
         conv2 = self.conv_bn_layer(
-            conv, 96, 3, stride=2, act='relu', name="inception_stem1_3x3_s2")
-        concat = fluid.layers.concat([pool1, conv2], axis=1)
+            conv, 96, 3, stride=2, act='relu', name="inception_stem1_3x3_s2", data_format=data_format)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([pool1, conv2], axis=1)
+        else:
+            concat = fluid.layers.concat([pool1, conv2], axis=3)
 
         conv1 = self.conv_bn_layer(
-            concat, 64, 1, act='relu', name="inception_stem2_3x3_reduce")
+            concat, 64, 1, act='relu', name="inception_stem2_3x3_reduce", data_format=data_format)
         conv1 = self.conv_bn_layer(
-            conv1, 96, 3, act='relu', name="inception_stem2_3x3")
+            conv1, 96, 3, act='relu', name="inception_stem2_3x3", data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            concat, 64, 1, act='relu', name="inception_stem2_1x7_reduce")
+            concat, 64, 1, act='relu', name="inception_stem2_1x7_reduce", data_format=data_format)
         conv2 = self.conv_bn_layer(
             conv2,
             64, (7, 1),
             padding=(3, 0),
             act='relu',
-            name="inception_stem2_1x7")
+            name="inception_stem2_1x7",
+            data_format=data_format,
+            data_format=data_format)
         conv2 = self.conv_bn_layer(
             conv2,
             64, (1, 7),
             padding=(0, 3),
             act='relu',
-            name="inception_stem2_7x1")
+            name="inception_stem2_7x1",
+            data_format=data_format)
         conv2 = self.conv_bn_layer(
-            conv2, 96, 3, act='relu', name="inception_stem2_3x3_2")
+            conv2, 96, 3, act='relu', name="inception_stem2_3x3_2", data_format=data_format)
 
-        concat = fluid.layers.concat([conv1, conv2], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([conv1, conv2], axis=1)
+        else:
+            concat = fluid.layers.concat([conv1, conv2], axis=3)
 
         conv1 = self.conv_bn_layer(
-            concat, 192, 3, stride=2, act='relu', name="inception_stem3_3x3_s2")
+            concat, 192, 3, stride=2, act='relu', name="inception_stem3_3x3_s2", data_format=data_format)
         pool1 = fluid.layers.pool2d(
-            input=concat, pool_size=3, pool_stride=2, pool_type='max')
+            input=concat, pool_size=3, pool_stride=2, pool_type='max', data_format=data_format)
 
-        concat = fluid.layers.concat([conv1, pool1], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([conv1, pool1], axis=1)
+        else:
+            concat = fluid.layers.concat([conv1, pool1], axis=3)
 
         return concat
 
-    def inceptionA(self, data, name=None):
+    def inceptionA(self, data, name=None, data_format='NCHW'):
         pool1 = fluid.layers.pool2d(
-            input=data, pool_size=3, pool_padding=1, pool_type='avg')
+            input=data, pool_size=3, pool_padding=1, pool_type='avg', data_format=data_format)
         conv1 = self.conv_bn_layer(
-            pool1, 96, 1, act='relu', name="inception_a" + name + "_1x1")
+            pool1, 96, 1, act='relu', name="inception_a" + name + "_1x1", data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            data, 96, 1, act='relu', name="inception_a" + name + "_1x1_2")
+            data, 96, 1, act='relu', name="inception_a" + name + "_1x1_2", data_format=data_format)
 
         conv3 = self.conv_bn_layer(
-            data, 64, 1, act='relu', name="inception_a" + name + "_3x3_reduce")
+            data, 64, 1, act='relu', name="inception_a" + name + "_3x3_reduce", data_format=data_format)
         conv3 = self.conv_bn_layer(
             conv3,
             96,
             3,
             padding=1,
             act='relu',
-            name="inception_a" + name + "_3x3")
+            name="inception_a" + name + "_3x3",
+            data_format=data_format)
 
         conv4 = self.conv_bn_layer(
             data,
             64,
             1,
             act='relu',
-            name="inception_a" + name + "_3x3_2_reduce")
+            name="inception_a" + name + "_3x3_2_reduce",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             96,
             3,
             padding=1,
             act='relu',
-            name="inception_a" + name + "_3x3_2")
+            name="inception_a" + name + "_3x3_2",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             96,
             3,
             padding=1,
             act='relu',
-            name="inception_a" + name + "_3x3_3")
+            name="inception_a" + name + "_3x3_3",
+            data_format=data_format)
 
-        concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=1)
+        else:
+            concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=3)
 
         return concat
 
-    def reductionA(self, data, name=None):
+    def reductionA(self, data, name=None, data_format='NCHW'):
         pool1 = fluid.layers.pool2d(
-            input=data, pool_size=3, pool_stride=2, pool_type='max')
+            input=data, pool_size=3, pool_stride=2, pool_type='max', data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            data, 384, 3, stride=2, act='relu', name="reduction_a_3x3")
+            data, 384, 3, stride=2, act='relu', name="reduction_a_3x3", data_format=data_format)
 
         conv3 = self.conv_bn_layer(
-            data, 192, 1, act='relu', name="reduction_a_3x3_2_reduce")
+            data, 192, 1, act='relu', name="reduction_a_3x3_2_reduce", data_format=data_format)
         conv3 = self.conv_bn_layer(
-            conv3, 224, 3, padding=1, act='relu', name="reduction_a_3x3_2")
+            conv3, 224, 3, padding=1, act='relu', name="reduction_a_3x3_2", data_format=data_format)
         conv3 = self.conv_bn_layer(
-            conv3, 256, 3, stride=2, act='relu', name="reduction_a_3x3_3")
+            conv3, 256, 3, stride=2, act='relu', name="reduction_a_3x3_3", data_format=data_format)
 
-        concat = fluid.layers.concat([pool1, conv2, conv3], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([pool1, conv2, conv3], axis=1)
+        else:
+            concat = fluid.layers.concat([pool1, conv2, conv3], axis=3)
 
         return concat
 
-    def inceptionB(self, data, name=None):
+    def inceptionB(self, data, name=None, data_format='NCHW'):
         pool1 = fluid.layers.pool2d(
-            input=data, pool_size=3, pool_padding=1, pool_type='avg')
+            input=data, pool_size=3, pool_padding=1, pool_type='avg', data_format=data_format)
         conv1 = self.conv_bn_layer(
-            pool1, 128, 1, act='relu', name="inception_b" + name + "_1x1")
+            pool1, 128, 1, act='relu', name="inception_b" + name + "_1x1", data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            data, 384, 1, act='relu', name="inception_b" + name + "_1x1_2")
+            data, 384, 1, act='relu', name="inception_b" + name + "_1x1_2", data_format=data_format)
 
         conv3 = self.conv_bn_layer(
-            data, 192, 1, act='relu', name="inception_b" + name + "_1x7_reduce")
+            data, 192, 1, act='relu', name="inception_b" + name + "_1x7_reduce", data_format=data_format)
         conv3 = self.conv_bn_layer(
             conv3,
             224, (1, 7),
             padding=(0, 3),
             act='relu',
-            name="inception_b" + name + "_1x7")
+            name="inception_b" + name + "_1x7",
+            data_format=data_format)
         conv3 = self.conv_bn_layer(
             conv3,
             256, (7, 1),
             padding=(3, 0),
             act='relu',
-            name="inception_b" + name + "_7x1")
+            name="inception_b" + name + "_7x1",
+            data_format=data_format)
 
         conv4 = self.conv_bn_layer(
             data,
             192,
             1,
             act='relu',
-            name="inception_b" + name + "_7x1_2_reduce")
+            name="inception_b" + name + "_7x1_2_reduce",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             192, (1, 7),
             padding=(0, 3),
             act='relu',
-            name="inception_b" + name + "_1x7_2")
+            name="inception_b" + name + "_1x7_2",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             224, (7, 1),
             padding=(3, 0),
             act='relu',
-            name="inception_b" + name + "_7x1_2")
+            name="inception_b" + name + "_7x1_2",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             224, (1, 7),
             padding=(0, 3),
             act='relu',
-            name="inception_b" + name + "_1x7_3")
+            name="inception_b" + name + "_1x7_3",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             256, (7, 1),
             padding=(3, 0),
             act='relu',
-            name="inception_b" + name + "_7x1_3")
+            name="inception_b" + name + "_7x1_3",
+            data_format=data_format)
 
-        concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=1)
+        else:
+            concat = fluid.layers.concat([conv1, conv2, conv3, conv4], axis=3)
 
         return concat
 
-    def reductionB(self, data, name=None):
+    def reductionB(self, data, name=None, data_format='NCHW'):
         pool1 = fluid.layers.pool2d(
-            input=data, pool_size=3, pool_stride=2, pool_type='max')
+            input=data, pool_size=3, pool_stride=2, pool_type='max', data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            data, 192, 1, act='relu', name="reduction_b_3x3_reduce")
+            data, 192, 1, act='relu', name="reduction_b_3x3_reduce", data_format=data_format)
         conv2 = self.conv_bn_layer(
-            conv2, 192, 3, stride=2, act='relu', name="reduction_b_3x3")
+            conv2, 192, 3, stride=2, act='relu', name="reduction_b_3x3", data_format=data_format)
 
         conv3 = self.conv_bn_layer(
-            data, 256, 1, act='relu', name="reduction_b_1x7_reduce")
+            data, 256, 1, act='relu', name="reduction_b_1x7_reduce", data_format=data_format)
         conv3 = self.conv_bn_layer(
             conv3,
             256, (1, 7),
             padding=(0, 3),
             act='relu',
-            name="reduction_b_1x7")
+            name="reduction_b_1x7",
+            data_format=data_format)
         conv3 = self.conv_bn_layer(
             conv3,
             320, (7, 1),
             padding=(3, 0),
             act='relu',
-            name="reduction_b_7x1")
+            name="reduction_b_7x1",
+            data_format=data_format)
         conv3 = self.conv_bn_layer(
-            conv3, 320, 3, stride=2, act='relu', name="reduction_b_3x3_2")
+            conv3, 320, 3, stride=2, act='relu', name="reduction_b_3x3_2", data_format=data_format)
 
-        concat = fluid.layers.concat([pool1, conv2, conv3], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat([pool1, conv2, conv3], axis=1)
+        else:
+            concat = fluid.layers.concat([pool1, conv2, conv3], axis=3)
 
         return concat
 
-    def inceptionC(self, data, name=None):
+    def inceptionC(self, data, name=None, data_format='NCHW'):
         pool1 = fluid.layers.pool2d(
-            input=data, pool_size=3, pool_padding=1, pool_type='avg')
+            input=data, pool_size=3, pool_padding=1, pool_type='avg', data_format=data_format)
         conv1 = self.conv_bn_layer(
-            pool1, 256, 1, act='relu', name="inception_c" + name + "_1x1")
+            pool1, 256, 1, act='relu', name="inception_c" + name + "_1x1", data_format=data_format)
 
         conv2 = self.conv_bn_layer(
-            data, 256, 1, act='relu', name="inception_c" + name + "_1x1_2")
+            data, 256, 1, act='relu', name="inception_c" + name + "_1x1_2", data_format=data_format)
 
         conv3 = self.conv_bn_layer(
-            data, 384, 1, act='relu', name="inception_c" + name + "_1x1_3")
+            data, 384, 1, act='relu', name="inception_c" + name + "_1x1_3", data_format=data_format)
         conv3_1 = self.conv_bn_layer(
             conv3,
             256, (1, 3),
             padding=(0, 1),
             act='relu',
-            name="inception_c" + name + "_1x3")
+            name="inception_c" + name + "_1x3",
+            data_format=data_format)
         conv3_2 = self.conv_bn_layer(
             conv3,
             256, (3, 1),
             padding=(1, 0),
             act='relu',
-            name="inception_c" + name + "_3x1")
+            name="inception_c" + name + "_3x1",
+            data_format=data_format)
 
         conv4 = self.conv_bn_layer(
-            data, 384, 1, act='relu', name="inception_c" + name + "_1x1_4")
+            data, 384, 1, act='relu', name="inception_c" + name + "_1x1_4", data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             448, (1, 3),
             padding=(0, 1),
             act='relu',
-            name="inception_c" + name + "_1x3_2")
+            name="inception_c" + name + "_1x3_2",
+            data_format=data_format)
         conv4 = self.conv_bn_layer(
             conv4,
             512, (3, 1),
             padding=(1, 0),
             act='relu',
-            name="inception_c" + name + "_3x1_2")
+            name="inception_c" + name + "_3x1_2",
+            data_format=data_format)
         conv4_1 = self.conv_bn_layer(
             conv4,
             256, (1, 3),
             padding=(0, 1),
             act='relu',
-            name="inception_c" + name + "_1x3_3")
+            name="inception_c" + name + "_1x3_3",
+            data_format=data_format)
         conv4_2 = self.conv_bn_layer(
             conv4,
             256, (3, 1),
             padding=(1, 0),
             act='relu',
-            name="inception_c" + name + "_3x1_3")
+            name="inception_c" + name + "_3x1_3",
+            data_format=data_format)
 
-        concat = fluid.layers.concat(
-            [conv1, conv2, conv3_1, conv3_2, conv4_1, conv4_2], axis=1)
+        if data_format == 'NCHW':
+            concat = fluid.layers.concat(
+                [conv1, conv2, conv3_1, conv3_2, conv4_1, conv4_2], axis=1)
+        else:
+            concat = fluid.layers.concat(
+                [conv1, conv2, conv3_1, conv3_2, conv4_1, conv4_2], axis=3)
 
         return concat
--- PaddleCV/image_classification/scripts/train/InceptionV4.sh
+++ PaddleCV/image_classification/scripts/train/InceptionV4.sh
@@ -4,6 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export FLAGS_fast_eager_deletion_mode=1
 export FLAGS_eager_delete_tensor_gb=0.0
 export FLAGS_fraction_of_gpu_memory_to_use=0.98
+export FLAGS_cudnn_batchnorm_spatial_persistent=1
 
 #InceptionV4
 python train.py \
@@ -21,3 +22,4 @@ python train.py \
             --resize_short_size=320 \
             --use_label_smoothing=True \
             --label_smoothing_epsilon=0.1 \
+            --data_format=NCHW
